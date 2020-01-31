cfa0f91c16c7f7040476b7902c0776e8203b32a4
Vianna <vianna8523@gmail.com>
2020-01-31 21:28:52 -0200

    Preparando o React js

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './main/App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ frontend/src/main/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './App.css';
+
+export default props => (
+  <div className='container'>
+    <h1>Teste</h1>
+  </div>
+);
