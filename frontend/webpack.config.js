5709666f96493b1c38743a41d974b59f1e91ff75
Vianna <vianna8523@gmail.com>
2020-01-31 21:28:52 -0200

    Preparando o ambiente React com html e js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -15,26 +15,25 @@ module.exports = {
         extensions: ['', '.js'],
         alias: {
             modules: __dirname + '/node_modules'
-        },
-        plugins: [
-            new ExtractTextPlugin('app.css')
-        ],
-        module: {
-            loaders: [{
-                test: /.js[x]:$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react'],
-                    plugins: ['transform-object-rest-spread']
-                }
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            }, {
-                test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
-                loader: 'file'
-            }]
         }
+    },
+    plugins: [ 
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
     }
 }
